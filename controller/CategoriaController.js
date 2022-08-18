abeb60e922d5698ef90a0594e19d7c2d21b8fd82
cristianocorrea3 <66446324+cristianocorrea3@users.noreply.github.com>
2022-08-18 07:40:00 -0300

    Refatoração dos nomes das rotas de inserção, seleção, alteração e exclusão de dados referentes a categoria. Implementação dos códigos de seleção de dados de categoria sem critérios, implementação dos códigos de seleção de dados de categoria com critério por "id", implementação dos códigos de alteração de dados de categoria com critério por "id" e implementação dos códigos de exclusão de dados de categoria com critério por "id".
    Refatoração dos nomes das rotas de inserção, seleção, alteração e exclusão de dados referentes a categoria. Implementação dos códigos de seleção de dados de categoria sem critérios, implementação dos códigos de seleção de dados de categoria com critério por "id", implementação dos códigos de alteração de dados de categoria com critério por "id" e implementação dos códigos de exclusão de dados de categoria com critério por "id".

--- controller/CategoriaController.js
+++ controller/CategoriaController.js
@@ -7,58 +7,145 @@ const router = express.Router();
 const modelCategoria = require('../model/CategoriaModel');
 
 /* PARAMETROS DE ROTAS (QUALQUER VERBO):
-1 - NOME DA ROTA
+1 - NOME DA ROTA - REPRESENTADO POR UMA STRING
 2 - CALLBACK QUE TRATA REQUISIÇÃO (req) E RESPOSTA (res)
 */
 /*ROTAS DE CRUD DE CATEGORIAS:*/
-router.get('/testeget', (req, res)=>{
+router.get('/listarCategoria', (req, res)=>{
+
     // console.log('TESTE DE ROTA GET DE CATEGORIAS');
-    console.log('----A REQUISIÇÃO GET PASSOU PELA CATEGORIA CONTROLLER----');
-    res.send('----TESTE DE ROTA GET DE CATEGORIAS----');
+    // console.log('----A REQUISIÇÃO GET PASSOU PELA CATEGORIA CONTROLLER----');
+    // res.send('----TESTE DE ROTA GET DE CATEGORIAS----');
+
+    //LISTANDO OS DADOS SEM CRITÉRIOS
+    modelCategoria.findAll()
+        .then(
+            (categorias)=>{
+                return res.status(200).json(categorias);
+            }
+        ).catch(
+            (erro)=>{
+                return res.status(400).json({
+                    erroStatus: true,
+                    erroMessagem: 'Houve um erro ao selecionar os dados de categoria',
+                    erroBancoDados: erro
+                });
+            }
+        );
+
 });
 
-router.post('/testepost', (req, res)=>{
+//LISTANDO OS DADOS COM CRITÉRIOS
+router.get('/listarCategoria/:id',(req, res)=>{
+
+    let {id} = req.params;
+
+    modelCategoria.findByPk(id)
+        .then(
+            (categoria)=>{
+                res.status(200).json(categoria);
+            }
+        ).catch(
+            (erro)=>{
+                return res.status(400).json({
+                    erroStatus: true,
+                    erroMessagem: 'Houve um erro ao selecionar os dados de categoria',
+                    erroBancoDados: erro
+                });
+            }
+        );
+
+});
+
+router.post('/inserirCategoria', (req, res)=>{
     // console.log('A REQUISIÇÃO POST PASSOU PELA CATEGORIA CONTROLLER');
     // res.send('TESTE DE ROTA POST DE CATEGORIAS');
 
     //RECEBER OS DADOS
-    // console.log(req);
-    // let nome = req.body.nome_categoria;
+    // console.log(req.body.nome_categoria);
+    // let nome_categoria = req.body.nome_categoria;
     let {nome_categoria} = req.body;
-    console.log(nome_categoria);
+    // console.log(nome_categoria);
     
+    //GRAVAR OS DADOS
     modelCategoria.create(
         {nome_categoria}
     ).then(
         ()=>{
-            return res.status(201).json({
-                erroStatus: false,
-                menssagemStatus: 'Categoria inserida com sucesso!'
+                return res.status(201).json({
+                    erroStatus: false,
+                    menssagemStatus: 'Categoria inserida com sucesso!'
             });
         }
     ).catch(
         (erro)=>{
-            return res.status(400).json({
-                erroStatus: true,
-                erroMessagem: 'Houve um erro ao cadastrar a categoria',
-                erroBancoDados: erro
-            });
+                    return res.status(400).json({
+                        erroStatus: true,
+                        erroMessagem: 'Houve um erro ao cadastrar a categoria',
+                        erroBancoDados: erro
+                    });
         }
     );
 
+});
 
-    //GRAVAR OS DADOS
+router.put('/alterarCategoria', (req, res)=>{
+
+    // console.log('A REQUISIÇÃO PUT PASSOU PELA CATEGORIA CONTROLLER');
+    // res.send('TESTE DE ROTA PUT DE CATEGORIAS');
+
+    //RECEBENDO OS DADOS:
+    let {id, nome_categoria} = req.body;
+
+    //ALTERANDO OS DADOS:
+    modelCategoria.update(
+        {nome_categoria},
+        {where:{id}}
+    ).then( ()=>{
+
+        return res.status(200).json({
+            erroStatus: false,
+            menssagemStatus: 'Categoria alterada com sucesso!'
+        });
+
+    }).catch(
+        (erro)=>{
+                    return res.status(400).json({
+                        erroStatus: true,
+                        erroMessagem: 'Houve um erro ao alterar a categoria',
+                        erroBancoDados: erro
+                    });
+        }
+    );
 
 });
 
-router.put('/testeput', (req, res)=>{
-    console.log('A REQUISIÇÃO PUT PASSOU PELA CATEGORIA CONTROLLER');
-    res.send('TESTE DE ROTA PUT DE CATEGORIAS');
-});
+router.delete('/excluirCategoria/:id', (req, res)=>{
 
-router.delete('/testedelete', (req, res)=>{
-    console.log('A REQUISIÇÃO DELETE PASSOU PELA CATEGORIA CONTROLLER');
-    res.send('TESTE DE ROTA DELETE DE CATEGORIAS');
+    // console.log('A REQUISIÇÃO DELETE PASSOU PELA CATEGORIA CONTROLLER');
+    // res.send('TESTE DE ROTA DELETE DE CATEGORIAS');
+
+    let {id} = req.params;
+
+    modelCategoria.destroy(
+        {where: {id}}
+    ).then( ()=>{
+
+        return res.status(200).json({
+            erroStatus: false,
+            menssagemStatus: 'Categoria excluida com sucesso!'
+        });
+
+    }).catch(
+        (erro)=>{
+                    return res.status(400).json({
+                        erroStatus: true,
+                        erroMessagem: 'Houve um erro ao excluir a categoria',
+                        erroBancoDados: erro
+                    });
+        }
+    );
+
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 /* ROTAS DE CATEGORIA DE LIVROS: */
-console.log('A REQUISIÇÃO PASSOU PELA INDEX');
+// console.log('A REQUISIÇÃO PASSOU PELA INDEX');
 app.use('/', categoriaController);
 
 /* 
